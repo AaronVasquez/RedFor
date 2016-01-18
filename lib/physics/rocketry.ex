95db0931de65e6e54b23606ee8b4a07f05baea62
Aaron Vasquez <aaronvasquez1@gmail.com>
2016-01-18 11:06:32 -0500

    generalizes orbital_radius

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -2,16 +2,17 @@ defmodule Physics.Rocketry do
   import Calcs
   import Physics.Laws
 
-  def escape_velocity(), do: Planets.earth |> escape_velocity
+  def escape_velocity(),       do: :earth |> escape_velocity
   def escape_velocity(:earth), do: Planets.earth |> escape_velocity
-  def escape_velocity(:mars), do: Planets.mars |> escape_velocity
-  def escape_velocity(:moon), do: Planets.moon |> escape_velocity
+  def escape_velocity(:mars),  do: Planets.mars |> escape_velocity
+  def escape_velocity(:moon),  do: Planets.moon |> escape_velocity
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
       |> to_km
       |> to_nearest_tenth
   end
+
 
   def orbital_speed(height) do
     newtons_gravitational_constant * Planets.earth.mass / orbital_radius(height)
@@ -36,8 +37,10 @@ defmodule Physics.Rocketry do
       |> square_root
   end
 
-  def orbital_radius(height) do
-    Planets.earth.radius + (height |> to_m)
-  end
+  defp orbital_radius(height),         do: :earth |> orbital_radius(height)
+  defp orbital_radius(:earth, height), do: Planets.earth |> orbital_radius(height)
+  defp orbital_radius(:mars, height),  do: Planets.mars |> orbital_radius(height)
+  defp orbital_radius(:moon, height),  do: Planets.moon |> orbital_radius(height)
+  defp orbital_radius(planet, height), do: planet.radius + (height |> to_m)
 
 end
